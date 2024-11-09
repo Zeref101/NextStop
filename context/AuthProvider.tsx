70b3bd22b02a8060be695b640911335242ccd9f4
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-10 01:56:14 +0530

    Enhance AuthProvider to manage user state and loading status with current user retrieval

--- context/AuthProvider.tsx
+++ context/AuthProvider.tsx
@@ -1,20 +1,39 @@
+import { getCurrentUser } from "@/lib/appwrite";
 import { createContext, useState, useContext, Children } from "react";
 import { ReactNode } from "react";
+import React from "react";
+import { UserProps } from "@/types";
 
 const AuthContext = createContext({
-    isLoggedIn: false,
-    login: () => { },
-    logout: () => { }
+
 });
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState<UserProps | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
 
-    const login = () => setIsLoggedIn(true);
-    const logout = () => setIsLoggedIn(false);
+    React.useEffect(() => {
+        getCurrentUser()
+            .then((res) => {
+                if (res) {
+                    setIsLoggedIn(true);
+                    setUser(res);
+                } else {
+                    setIsLoggedIn(false);
+                    setUser(null);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }, [])
 
     return (
-        <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, user, setIsLoading, setUser }}>
             {children}
         </AuthContext.Provider>
     )
