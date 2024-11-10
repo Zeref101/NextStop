6c8b7b2bd4628dc5e964bef10565c60a5ee0ee42
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-10 17:11:29 +0530

    Enhance Home screen layout with SafeAreaView, ScrollView, and SearchInput component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,14 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
 import { useAuth } from '@/context/AuthProvider'
+import { SafeAreaView } from 'react-native-safe-area-context';
+import images from '@/constants/images';
+import SearchInput from '@/components/SearchInput';
 
 const Home = () => {
     const { user } = useAuth();
 
     return (
-        <View>
-            <Text>Hello {user?.username}</Text>
-        </View>
+        <SafeAreaView className=' bg-primary h-full'>
+            <ScrollView>
+                <View className='flex-1 justify-center gap-4 p-4 mt-4'>
+                    <View className=' flex-row justify-between'>
+                        <View className=' flex-1 gap-2'>
+                            <Text className='text-base font-pregular text-white'>Welcome Back</Text>
+                            <Text className=' font-psemibold text-[22px] text-white'>{user?.username}</Text>
+                        </View>
+                        <Image
+                            source={require("../../assets/images/logo-small-3.png")}
+                        />
+                    </View>
+                    <View>
+                        <SearchInput />
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
