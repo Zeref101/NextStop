b4f055de6d64e5512aeb0719fe86aa16be3c2124
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-10 09:03:10 +0530

    Wrap RootLayout in AuthProvider to manage authentication state

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import { View, Text } from 'react-native'
 import { Stack } from 'expo-router'
 import React from 'react'
+import { AuthProvider } from '@/context/AuthProvider'
 
 const RootLayout = () => {
     return (
         <>
-            <Stack>
-                <Stack.Screen name='index' options={{ headerShown: false }} />
-                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
+            <AuthProvider>
+                <Stack>
+                    <Stack.Screen name='index' options={{ headerShown: false }} />
+                    <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+                    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                </Stack>
+            </AuthProvider>
         </>
     )
 }
