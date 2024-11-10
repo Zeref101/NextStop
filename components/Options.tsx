2d6a14061a97e263721ebc7f70c268ae52fc9462
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-11 03:05:19 +0530

    Update Options component to adjust text color based on state selection

--- components/Options.tsx
+++ components/Options.tsx
@@ -6,13 +6,13 @@ const Options = ({ handleStateChange, state }: { handleStateChange: (newState: V
     return (
         <View className={`h-[50px] mx-8  flex-row justify-between rounded-xl border border-[#7b7b8b]`}>
             <TouchableOpacity onPress={() => handleStateChange(ViewState.Places)} className="flex-1 h-full justify-center items-center border-r-2 border-[#7b7b8b]">
-                <Text className={`text-center text-base text-white font-pmedium ${state === ViewState.Places ? "text-secondary" : ""}`}>Places</Text>
+                <Text className={`text-center text-base  font-pmedium ${state === ViewState.Places ? "text-secondary" : "text-white"}`}>Places</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => handleStateChange(ViewState.Food)} className="flex-1 h-full justify-center items-center border-r-2 border-[#7b7b8b]">
-                <Text className={`text-center text-base text-white font-pmedium ${state === ViewState.Food ? "text-secondary" : ""}`}>Food</Text>
+                <Text className={`text-center text-base font-pmedium ${state === ViewState.Food ? "text-secondary" : "text-white"}`}>Food</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => handleStateChange(ViewState.BestTime)} className="flex-1 h-full justify-center items-center">
-                <Text className={`text-center text-base text-white font-pmedium ${state === ViewState.BestTime ? "text-secondary" : ""}`}>Best Time</Text>
+                <Text className={`text-center text-base font-pmedium ${state === ViewState.BestTime ? "text-secondary" : "text-white"}`}>Best Time</Text>
             </TouchableOpacity>
         </View>
     )
