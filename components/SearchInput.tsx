dbb67ee7f9cf9a8e84fbd4349f5ad593f6ca855f
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-10 17:11:49 +0530

    Add SearchInput component for search functionality

---
+++ components/SearchInput.tsx
@@ -0,0 +1,15 @@
+import { View, Text, TextInput } from 'react-native'
+import React from 'react'
+
+const SearchInput = () => {
+    const [isFocused, setIsFocused] = React.useState(false);
+    return (
+        <>
+
+
+        </>
+    )
+}
+
+export default SearchInput
+
