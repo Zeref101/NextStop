28b984d3b63fa3cc6c1d75d08c54c8e5682b656a
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-10 09:03:54 +0530

    Add tabScreens constant for dynamic tab navigation

---
+++ constants/index.ts
@@ -0,0 +1,8 @@
+import icons from "./icons";
+
+export const tabScreens = [
+  { name: "home", title: "Home", icon: icons.home },
+  { name: "search", title: "Search", icon: icons.search },
+  { name: "profile", title: "Profile", icon: icons.profile },
+  { name: "save", title: "Save", icon: icons.bookmark },
+];
