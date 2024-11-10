ffa17d4e79c6430fbe9bfba1eae025eadc0d2d71
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-10 17:11:39 +0530

    Refactor Save screen to use SafeAreaView and ScrollView for improved layout

--- app/(tabs)/save.tsx
+++ app/(tabs)/save.tsx
@@ -1,11 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const save = () => {
     return (
-        <View>
-            <Text>save</Text>
-        </View>
+        <SafeAreaView className=' bg-primary h-full'>
+            <ScrollView>
+                <View>
+                    <Text>
+                        Saved Places
+                    </Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
