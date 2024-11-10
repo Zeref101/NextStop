d45d9764a84819b2841ee328352aebd172436135
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-10 09:03:36 +0530

    Display personalized greeting with username in Home component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { useAuth } from '@/context/AuthProvider'
 
 const Home = () => {
+    const { user } = useAuth();
+
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Hello {user?.username}</Text>
         </View>
     )
 }
