ef3d709bb461741d31079497715663f710abdea7
Zeref101 <shreyas.mohanty03@gmail.com>
2024-11-11 14:17:36 +0530

    Enhance Profile component with user information display and logout functionality

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,55 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, Image, Pressable } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useAuth } from '@/context/AuthProvider'
+import icons from '@/constants/icons'
+import { signOut } from '@/lib/appwrite'
+import { Redirect } from 'expo-router'
+
 
 const Profile = () => {
+    const { user } = useAuth();
+
+    const handleLogout = async () => {
+        const deleteSession = await signOut();
+        if (deleteSession) {
+            return (
+                <Redirect href={'/sign-in'} />
+            )
+        }
+    }
     return (
         <SafeAreaView className=' bg-primary h-full'>
             <ScrollView>
-                <View>
-                    <Text>
-                        Profile
-                    </Text>
+                <View className=' w-full h-[85vh] justify-start p-4'>
+                    <View className=' w-full flex flex-row justify-between'>
+
+                        <Text className='text-white font-pmedium text-3xl'>
+                            Profile
+                        </Text>
+                        <Pressable onPress={handleLogout}>
+
+                        </Pressable>
+                        <Image source={icons.logout} className=' w-6 h-6' resizeMode='contain' />
+                    </View>
+                    <View className=' w-full h-full flex justify-start items-center'>
+
+                        <View className=' w-[80%] p-4 bg-black-100  justify-start items-center mt-7 rounded-xl'>
+                            <View className=' flex flex-col justify-center items-center gap-2.5'>
+                                <Image source={{ uri: user?.avatar }} className=' w-24 h-24 rounded-xl border-2 border-secondary-100' resizeMode='contain' />
+                                <Text className=' text-white font-psemibold text-[20px]'>{user?.username}</Text>
+                            </View>
+                            <View className=' w-full items-start m-2'>
+
+                                <Text className=' text-base font-pmedium text-secondary'>
+                                    email {" "}: <Text className='text-base font-pmedium text-white'>{user?.email}</Text>
+                                </Text>
+
+                            </View>
+
+                        </View>
+                    </View>
+
                 </View>
             </ScrollView>
         </SafeAreaView>
